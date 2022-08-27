fde9960433347da614ea022d9d85794e9a4a0349
chinedugabriel <chinedugabriel29@gmail.com>
2022-08-27 19:45:05 +0100

    added a favicon and remove a text

--- page.js
+++ page.js
@@ -6,17 +6,14 @@ let icon_btn_toggle= document.getElementById("click_preView");
 
 // this function help to toggle the view of input password when a user clicks on the eye icon in the login form section 
 function preView (){
-    if(icon_btn_toggle.hasAttribute("name")){
-        console.log("working");
+    if(input_password.type == "password"){
         icon_view.src="img/visibility_img.png";
         input_password.type = "text";
-        icon_btn_toggle.removeAttribute("name")
     }else{
         icon_view.src="img/visibility_off_img.png";
-        input_password.type = "password"
-        icon_btn_toggle.setAttribute("name", "true")
+        input_password.type = "password";
     }
 }
-
+// this event calls the function preView to toggle the password view
 icon_btn_toggle.addEventListener("click", preView);
 
